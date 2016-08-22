9060e7be8ebfa92c9df27f68d17f84e10f67435c
YangJian <229201093@qq.com>
2016-08-22 13:51:44 +0800

    Initial Commit

---
+++ YJNSOperationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJNSOperationDemo
+//
+//  Created by GongHui_YJ on 16/8/22.
+//  Copyright © 2016年 YangJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
